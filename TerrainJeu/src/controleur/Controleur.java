ce2320b8bffb9179df6626657584d79dfe9d2a21
bseydi <101578786+bseydi@users.noreply.github.com>
2022-05-16 14:29:50 +0200

    Add files via upload

--- TerrainJeu/src/controleur/Controleur.java
+++ TerrainJeu/src/controleur/Controleur.java
@@ -3,11 +3,16 @@ package controleur;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import applicationV1.modele.Personnage;
 import applicationV1.modele.Terrain;
+import applicationV1.vue.PersonnageVue;
 import applicationV1.vue.TerrainVue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
@@ -15,37 +20,39 @@ import javafx.scene.shape.Circle;
 
 public class Controleur implements Initializable {
 	Terrain terrain;
-	ImageView img;
-	Personnage p;
-
+	Personnage personnage;
+	
+	TerrainVue terrainVue;
+	PersonnageVue personnageVue;
+	
 	@FXML
     private TilePane terrainJeu;
 	
 	@FXML
     private Pane panneauJeu;
 	
-	
+	@FXML
+    void toucheAppuyée(KeyEvent event) {
+		if(event.getCode()==KeyCode.D) {
+    		personnage.seDeplacerADroite(); 
+    	}
+    	else if(event.getCode()==KeyCode.Q) {
+    		personnage.seDeplacerAGauche();   		
+    	}
+    }
+		
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		this.terrain = new Terrain (20,20);
-		TerrainVue terrainVue = new TerrainVue(terrainJeu, terrain, panneauJeu);
+		terrainVue = new TerrainVue(terrainJeu, terrain);
+		personnage = new Personnage(100,172,5);
+		personnageVue = new PersonnageVue(panneauJeu);
 		try {
 			terrainVue.creerTerrainJeu();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
-	}
-	
-		@FXML
-    void keyPressed(KeyEvent e) {
-    	if(e.getCode()==KeyCode.D) {
-    		p.seDeplacerADroite(); 
-    		System.out.println("droite");
-    	}
-    	else if(e.getCode()==KeyCode.Q) {
-    		p.seDeplacerAGauche();
-    		System.out.println("gauche");
-    	}
-    }
+		}
+		personnageVue.perso(personnage);
+	}		
 }
