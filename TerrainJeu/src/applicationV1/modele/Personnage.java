99d4b421ff6b830e5ae13fdba0e5f152e454b4a0
SoleneLct <101125740+SoleneLct@users.noreply.github.com>
2022-05-16 12:01:49 +0200

    Add files via upload

--- TerrainJeu/src/applicationV1/modele/Personnage.java
+++ TerrainJeu/src/applicationV1/modele/Personnage.java
@@ -1,13 +1,73 @@
-package applicationV1.modele;
+package applicationV1;
+
+import java.util.Scanner;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.input.KeyEvent;
 
 public class Personnage {
-	private int x,y;
-	private int dx,dy;
+	private IntegerProperty xProperty,yProperty;
 	private int vitesse;
+	private int dx,dy ;// direction 
+	private Environnement env;
+	private int pointDeVie;
 	
-	public Personnage() {
-		
+	Personnage (int x,int y,int v){
+		this.xProperty=new SimpleIntegerProperty(x);
+		this.yProperty =new SimpleIntegerProperty(y);
+		this.vitesse=v;
+		this.pointDeVie=100;
 	}
 	
+	public void seDeplacerAGauche() {
+		this.xProperty.set(this.getX()-this.vitesse);
+	}
 	
+	public void seDeplacerADroite() {
+		this.xProperty.set(this.getX()+this.vitesse);
+	}
+	
+	public void seDeplacerEnHaut() {
+		this.yProperty.set(this.getY()+this.vitesse);
+	}
+	
+	public void seDeplacerEnBas() {
+		this.yProperty.set(this.getY()-this.vitesse);
+	}
+	
+	public boolean estVivant() {
+		return this.pointDeVie>0;
+	}
+
+	public final IntegerProperty xProperty() {
+		return xProperty;
+	}
+
+	public final void setX(int n){
+		xProperty.setValue(n);
+	}
+
+	public final int getX() {
+		return this.xProperty.getValue();
+	}
+	
+	public final IntegerProperty yProperty() {
+		return yProperty;
+	}
+	public final void setY(int n){
+		yProperty.setValue(n);
+	}
+	
+	public final int getY() {
+		return this.yProperty.getValue();
+	}
+
+	public int getVitesse() {
+		return vitesse;
+	}
+
+	public void setVitesse(int vitesse) {
+		this.vitesse = vitesse;
+	}
 }
