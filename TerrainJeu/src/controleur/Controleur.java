f9fdcebfaa332991265b343702b84de023dfc581
bseydi <101578786+bseydi@users.noreply.github.com>
2022-05-16 16:55:52 +0200

    Add files via upload

--- TerrainJeu/src/controleur/Controleur.java
+++ TerrainJeu/src/controleur/Controleur.java
@@ -3,20 +3,19 @@ package controleur;
 import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
-
 import applicationV1.modele.Personnage;
 import applicationV1.modele.Terrain;
 import applicationV1.vue.PersonnageVue;
 import applicationV1.vue.TerrainVue;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.TilePane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class Controleur implements Initializable {
 	Terrain terrain;
@@ -25,35 +24,55 @@ public class Controleur implements Initializable {
 	TerrainVue terrainVue;
 	PersonnageVue personnageVue;
 	
+	private Timeline gameLoop;
+	private int temps;
+	
 	@FXML
     private TilePane terrainJeu;
 	
 	@FXML
     private Pane panneauJeu;
-	
-	@FXML
-    void toucheAppuyée(KeyEvent event) {
-		if(event.getCode()==KeyCode.D) {
-    		personnage.seDeplacerADroite(); 
-    	}
-    	else if(event.getCode()==KeyCode.Q) {
-    		personnage.seDeplacerAGauche();   		
-    	}
-    }
-		
+			
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		initAnimation();
+		gameLoop.play();
+		
 		this.terrain = new Terrain (20,20);
 		terrainVue = new TerrainVue(terrainJeu, terrain);
 		personnage = new Personnage(100,172,5);
 		personnageVue = new PersonnageVue(panneauJeu);
 		try {
 			terrainVue.creerTerrainJeu();
-			personnageVue.perso(personnage);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-	}		
+		personnageVue.perso(personnage);
+	}	
+	
+	private void initAnimation() {
+		gameLoop = new Timeline();
+		temps=0;
+		gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+		KeyFrame kf = new KeyFrame (Duration.seconds(1),(ev ->{
+			
+				personnage.setY(172);			
+				temps++;
+			})
+			);
+			gameLoop.getKeyFrames().add(kf);
+		}
+
+
+	@FXML
+    void toucheAppuyée(KeyEvent event) {
+		if(event.getCode()==KeyCode.D) {
+    		personnage.seDeplacerADroite(); 
+    	} else if(event.getCode()==KeyCode.Q) {
+    		personnage.seDeplacerAGauche();   		
+    	} else if(event.getCode()==KeyCode.Z) {
+    		 personnage.sauter();  		
+    	}
+    }
 }
