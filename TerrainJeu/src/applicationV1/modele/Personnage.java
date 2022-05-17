06df33d1e2420d64e573ff45fe037d2dc234728c
SoleneLct <101125740+SoleneLct@users.noreply.github.com>
2022-05-17 10:30:59 +0200

    Add files via upload

--- TerrainJeu/src/applicationV1/modele/Personnage.java
+++ TerrainJeu/src/applicationV1/modele/Personnage.java
@@ -5,17 +5,18 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 public class Personnage {
 	private IntegerProperty xProperty,yProperty;
-	private int vitesse; 
-	private int pointDeVie;
+	private int vitesse;
+	private IntegerProperty pointDeVie;
 	
 	public Personnage (int x,int y,int v){
-		super();
 		this.xProperty=new SimpleIntegerProperty(x);
 		this.yProperty =new SimpleIntegerProperty(y);
 		this.vitesse=v;
-		this.pointDeVie=100;
+		this.pointDeVie=new SimpleIntegerProperty(100);
 	}
 	
+
+
 	public void seDeplacerAGauche() {
 		this.xProperty.set(this.getX()-this.vitesse);
 	}
@@ -29,7 +30,7 @@ public class Personnage {
 	}
 	
 	public boolean estVivant() {
-		return this.pointDeVie>0;
+		return this.pointDeVie.get()>0;
 	}
 
 	public final IntegerProperty xProperty() {
@@ -54,7 +55,19 @@ public class Personnage {
 	public final int getY() {
 		return this.yProperty.getValue();
 	}
+	
+	public final IntegerProperty pointdeVie() {
+		return pointDeVie;
+	}
 
+	public void setPointDeVie(int n) {
+		pointDeVie.setValue(n);
+	}
+	
+	public final int getPointDeVie() {
+		return this.pointDeVie.getValue();
+	}
+	
 	public int getVitesse() {
 		return vitesse;
 	}
