35938b858f7629aeb6b856674b6e8ad230caa8bc
WoodnycaV <101125564+WoodnycaV@users.noreply.github.com>
2022-05-19 17:01:15 +0200

    Add files via upload

--- TerrainJeu/src/applicationV1/vue/VieVue.java
+++ TerrainJeu/src/applicationV1/vue/VieVue.java
@@ -32,6 +32,8 @@ public class VieVue {
 				case 0:
 					coeurVide();
 					break;
+				default:
+					break;
 					
 				}
 			}
@@ -41,21 +43,21 @@ public class VieVue {
 	public void coeur () throws FileNotFoundException {
 		FileInputStream input = new FileInputStream("/home/etudiants/info/wvincent/prive/S2/DOO/TerrainJeu/src/image/coeur.png");
 		ImageView img = new ImageView(new Image (input));
-        placeCoeur.getChildren().add(img);
+        this.placeCoeur.getChildren().add(img);
 		
 	}
 	
 	public void miCoeur () throws FileNotFoundException {
 		FileInputStream input = new FileInputStream("/home/etudiants/info/wvincent/prive/S2/DOO/TerrainJeu/src/image/coeur.png");
 		ImageView img = new ImageView(new Image (input));
-        placeCoeur.getChildren().add(img);
+        this.placeCoeur.getChildren().add(img);
 		
 	}
 	
 	public void coeurVide () throws FileNotFoundException {
 		FileInputStream input = new FileInputStream("/home/etudiants/info/wvincent/prive/S2/DOO/TerrainJeu/src/image/coeurVide.png");
 		ImageView img = new ImageView(new Image (input));
-        placeCoeur.getChildren().add(img);
+        this.placeCoeur.getChildren().add(img);
 		
 	}
 }
