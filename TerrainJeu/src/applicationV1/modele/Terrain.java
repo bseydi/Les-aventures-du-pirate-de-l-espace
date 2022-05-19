10340258ba202b8a680ec5e475b1ead6a45c5bf2
Aldriculteur <105204520+Aldriculteur@users.noreply.github.com>
2022-05-19 16:10:51 +0200

    Add files via upload

--- TerrainJeu/src/applicationV1/modele/Terrain.java
+++ TerrainJeu/src/applicationV1/modele/Terrain.java
@@ -2,69 +2,33 @@ package applicationV1.modele;
 
 
 public class Terrain {
-	private int longueur;
-	private int largeur;
-	private int[][] carte;
 
+	private int [] carte = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+			1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,
+			2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
+			3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,4,3,3,3,3,3,3,3,3,3,3,3,4,
+			3,3,3,3,3,3,3,5,3,3,3,3,3,6,3,3,3,3,3,3,3,3,3,3,4,3,3,6,6,3,
+			3,3,4,3,3,3,3,3,3,3,3,3,3,3,6,3,3,3,6,6,3,3,5,3,3,3,6,6,6,3,
+			3,3,3,3,6,3,3,3,3,3,5,3,3,6,6,6,3,4,3,6,6,3,3,3,3,3,6,6,3,3,
+			3,5,3,6,6,6,3,4,3,3,3,6,3,3,3,3,3,3,3,6,6,6,3,3,3,3,3,3,3,3,
+			3,3,3,6,6,6,3,3,3,6,6,6,6,3,3,3,3,4,3,3,6,6,3,4,3,4,3,3,4,3,
+			3,3,6,6,6,6,6,6,6,6,6,6,6,3,3,5,3,3,3,6,3,3,3,3,3,3,3,3,3,3};
 
-	public Terrain (int largeur, int longueur) {
-		carte = new int [largeur][longueur];
-		this.longueur = longueur;
-		this.largeur = largeur;
-		
-		for(int ligne = 0; ligne < carte.length ;ligne++) {
-			for (int colonne = 0; colonne < carte[ligne].length; colonne++) {
-				    if (ligne <= carte.length/2) {
-				    	carte[ligne][colonne] = 0;				    
-				    }else if (ligne > carte.length/2) {
-				    	carte[ligne][colonne] = 1;
-				    }
-				    if(ligne == carte.length/2 +1) {
-				    	carte[ligne][colonne] = 2;
-				    }
-			}			
-		}	
-	}
+	public Terrain () {
+
+	}		
 	
-	public void afficherTableau() {     
-		
-        for(int ligne = 0; ligne < this.carte.length ;ligne++) {
-            for (int colonne = 0; colonne < this.carte[ligne].length; colonne++) {
-                System.out.print(this.carte[ligne][colonne]);
-                System.out.print("|");
-            }
-            System.out.println();
-        }
-		
-	}
-
-	public int getLongueur() {
-		return longueur;
-	}
-
-
-	public void setLongueur(int longueur) {
-		this.longueur = longueur;
-	}
-
-
-	public int getLargeur() {
-		return largeur;
-	}
-
-
-	public void setLargeur(int largeur) {
-		this.largeur = largeur;
-	}
-
-
-	public int[][] getCarte() {
+	public int[] getCarte() {
 		return carte;
-	}
-
-
-	public void setCarte(int[][] terrain) {
-		this.carte = terrain;
-	}
-	
+	}	
 }
