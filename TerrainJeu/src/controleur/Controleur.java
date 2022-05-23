f03fc40046a66f6a6f906915b00fd4b6502d5933
bseydi <101578786+bseydi@users.noreply.github.com>
2022-05-23 14:16:59 +0200

    Update Controleur.java

--- TerrainJeu/src/controleur/Controleur.java
+++ TerrainJeu/src/controleur/Controleur.java
@@ -4,19 +4,17 @@ import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import applicationV1.modele.Personnage;
-import applicationV1.modele.RessourcesDeBase;
-import applicationV1.modele.Terrain;
-import applicationV1.modele.Vie;
+import applicationV1.modele.principal.Personnage;
+import applicationV1.modele.principal.Terrain;
+import applicationV1.modele.ressourcesDeBase.Bois;
+import applicationV1.modele.ressourcesDeBase.Fer;
 import applicationV1.vue.PersonnageVue;
-import applicationV1.vue.RessourcesDeBaseVue;
+import applicationV1.vue.RessourceVue;
 import applicationV1.vue.TerrainVue;
-import applicationV1.vue.VieVue;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
@@ -26,29 +24,22 @@ import javafx.util.Duration;
 public class Controleur implements Initializable {
 	Terrain terrain;
 	Personnage personnage;
-	Vie vie;
+	
 	TerrainVue terrainVue;
 	PersonnageVue personnageVue;
-	VieVue vieVue;
-	RessourcesDeBase ressourcesBase;
-	RessourcesDeBaseVue ressourcesDeBaseVue;
+	
 	private Timeline gameLoop;
+
 	private int temps;
+	
+	int direction = 0;
 	
 	@FXML
     private TilePane terrainJeu;
 	
 	@FXML
     private Pane panneauJeu;
-	
-	@FXML
-    private Pane placeCoeur;
-	
-   @FXML
-    private TilePane placeRessources;
-	
-	
-		
+			
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		initAnimation();
@@ -56,51 +47,63 @@ public class Controleur implements Initializable {
 		
 		this.terrain = new Terrain ();
 		terrainVue = new TerrainVue(terrainJeu, terrain);
-		personnage = new Personnage(100,172,5);
-		personnageVue = new PersonnageVue(panneauJeu);
-		personnage.setPointDeVie(100);
-		vie = new Vie(personnage.pointdeVieProperty());
-		vieVue = new VieVue(placeCoeur, vie);
-		ressourcesBase = new RessourcesDeBase();
-		ressourcesDeBaseVue = new RessourcesDeBaseVue(placeRessources,ressourcesBase.getRessourcesBase());
-		//tabCoeur.miseAjourTabCoeur();
+		personnage = new Personnage(300,290,5);
+		personnageVue = new PersonnageVue(panneauJeu,personnage);
 		
 		try {
 			terrainVue.creerTerrainJeu();
-			personnageVue.perso(personnage);
-			vieVue.afficheCoeur();
-			ressourcesDeBaseVue.afficheRessources();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		personnageVue.perso();
 		
 	}	
-	
-	@FXML
-    void toucheAppuyée(KeyEvent event) {
-		if(event.getCode()==KeyCode.D) {
-    		personnage.seDeplacerADroite(); 
-    	} else if(event.getCode()==KeyCode.Q) {
-    		personnage.seDeplacerAGauche();   		
-    	} else if(event.getCode()==KeyCode.Z) {
-    		 personnage.sauter();  		
-    	}
-    }
 	
 	private void initAnimation() {
 		gameLoop = new Timeline();
 		temps=0;
 		gameLoop.setCycleCount(Timeline.INDEFINITE);
-
-		KeyFrame kf = new KeyFrame (Duration.seconds(1),(ev ->{
+		
+		KeyFrame kf = new KeyFrame (Duration.seconds(0.017 ),(ev ->{	
+					
 			
-				personnage.setY(172);			
-				temps++;
+						
+			temps++;
 			})
 			);
-			gameLoop.getKeyFrames().add(kf);
+		gameLoop.getKeyFrames().add(kf);	
+	}
+	
+	@FXML
+    void toucheAppuyée(KeyEvent event) {		
+		if(event.getCode()==KeyCode.D) {	
+    		direction = 1;
+			personnage.seDeplacerADroite();
+    	} 
+		else if(event.getCode()==KeyCode.Q) {
+    		direction = 2;
+			personnage.seDeplacerAGauche(); 
+    	} 
+		else if(event.getCode()==KeyCode.Z) {   		
+			if (direction == 1) {
+				personnage.setY(personnage.getY() - 100);	
+				personnage.setX(personnage.getX() + 100);
+			} else if (direction == 2) {
+				personnage.setY(personnage.getY() - 100);	
+				personnage.setX(personnage.getX() - 100);
+			} else {
+				personnage.setY(personnage.getY() - 100);	
+			}
+				
 		}
+	}
+	
+	
+		
+		
+			
 }
+
 	
 	
 
