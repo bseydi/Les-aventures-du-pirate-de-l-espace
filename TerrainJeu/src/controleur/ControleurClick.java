aa687194421a24b29dc75353ad66e7ac70d0af9a
Aldriculteur <105204520+Aldriculteur@users.noreply.github.com>
2022-05-23 14:19:31 +0200

    Add files via upload

--- TerrainJeu/src/controleur/ControleurClick.java
+++ TerrainJeu/src/controleur/ControleurClick.java
@@ -1,6 +1,6 @@
-
 package controleur;
 
+import applicationV1.modele.Terrain;
 import javafx.event.EventHandler;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -8,18 +8,25 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 
 public class ControleurClick implements EventHandler<MouseEvent> {
-	int id;
+	private int idBlock;
+	private int numéro;
+	private Terrain terrain;
 	
-	public ControleurClick(int i) {
-		this.id= i;
+	public ControleurClick(Terrain t,int idBlock,int numéro ) {
+		this.idBlock= idBlock;
+		this.numéro = numéro;
+		this.terrain = t;
 	}
 
 	@Override
 	public void handle(MouseEvent event) {
 		ImageView img =(ImageView) event.getSource();
-		if ( event.getButton() == MouseButton.SECONDARY && (this.id == 2 || this.id == 3 )) {
+		if ( event.getButton() == MouseButton.SECONDARY && (this.idBlock == 2 || this.idBlock == 3 )) {
 			img.setImage(new Image("./image/Ciel.png"));
+			terrain.supprimerCase(numéro);
+			
 		}
+		
 	}
 
 }
