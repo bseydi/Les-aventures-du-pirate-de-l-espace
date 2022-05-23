49958f748ba654c57424a57f913abafeda9dd3be
bseydi <101578786+bseydi@users.noreply.github.com>
2022-05-23 14:26:47 +0200

    Update PersonnageVue.java

--- TerrainJeu/src/applicationV1/vue/PersonnageVue.java
+++ TerrainJeu/src/applicationV1/vue/PersonnageVue.java
@@ -3,7 +3,7 @@ package applicationV1.vue;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
-import applicationV1.modele.Personnage;
+import applicationV1.modele.principal.Personnage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -13,26 +13,21 @@ import javafx.scene.shape.Circle;
 public class PersonnageVue {
 	
 	private Pane panneauJeu;
-	
-	
-	public PersonnageVue(Pane panneauJeu) {
+	Personnage personnage;
+		
+	public PersonnageVue(Pane panneauJeu, Personnage personnage) {
 		super();
 		this.panneauJeu = panneauJeu;
+		this.personnage = personnage;
 	}
 
-	public void perso ( Personnage personnage) throws FileNotFoundException {
-		
-		/*FileInputStream input = new FileInputStream("/home/etudiants/info/wvincent/prive/S2/DOO/TerrainJeu/src/image/perso.jpg");
-		ImageView img = new ImageView(new Image (input));
-   	 	panneauJeu.getChildren().add(img);
-*/
-		
-		Circle r;
+	public void perso () {
+		/*Circle r;
 		r = new Circle(5);
-		r.setFill(Color.RED);
+		r.setFill(Color.RED);*/
+		ImageView r = new ImageView(new Image("./image/astronaute(2).jpeg"));
 		r.translateXProperty().bind(personnage.xProperty());
     	r.translateYProperty().bind(personnage.yProperty());
    	 	panneauJeu.getChildren().add(r);
-   	 	  	 
 	}
 }
