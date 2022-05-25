09513441609908ff909c3aea41caeb631b632a40
SoleneLct <101125740+SoleneLct@users.noreply.github.com>
2022-05-25 11:17:09 +0200

    Update RessourcesDeBase.java

--- TerrainJeu/src/applicationV1/modele/RessourcesDeBase.java
+++ TerrainJeu/src/applicationV1/modele/RessourcesDeBase.java
@@ -1,11 +1,12 @@
 package applicationV1.modele;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class RessourcesDeBase {
 
-	private Map<Ressources,Integer> ressourcesBase;
+	private ArrayList<Ressources> listRessources;
 	private Ressources bois;
 	private Ressources fer;
 	private Ressources fraise;
@@ -16,41 +17,44 @@ public class RessourcesDeBase {
 	private Ressources viandesDeCreaturesCuite;
 	
 	public RessourcesDeBase() {
-		this.ressourcesBase=new HashMap<>();
-		this.ressourcesBase.put("bois", 0);
-		this.ressourcesBase.put("fer", 0);
-		this.ressourcesBase.put("fraise", 2);
-		this.ressourcesBase.put("pierre", 0);
-		this.ressourcesBase.put("pommeDeTerre", 2);
-		this.ressourcesBase.put("viandesDeCreatures", 0);
-		this.ressourcesBase.put("pommeDeTerreCuite", 0);
-		this.ressourcesBase.put("viandesDeCreaturesCuite", 0);
+		listRessources =  new ArrayList<Ressources>();
+		this.bois=new Bois();
+		this.fer = new Fer();
+		this.pierre = new Pierre();
+		this.listRessources.add(bois);
+		this.listRessources.add(fer);
+		this.listRessources.add(fraise);
+		this.listRessources.add(pierre);
+		this.listRessources.add(pommeDeTerre);
+		this.listRessources.add(viandesDeCreatures);
+		this.listRessources.add(pommeDeTerreCuite);
+		this.listRessources.add(viandesDeCreaturesCuite);
 	}
 	
 	
 
-	public Map<Ressources,Integer> getRessourcesBase() {
-		return ressourcesBase;
+	public ArrayList<Ressources> getRessourcesBase() {
+		return listRessources;
 	}
 
-	public void setRessourcesBase(Map<Ressources,Integer> ressourcesBase) {
-		this.ressourcesBase = ressourcesBase;
+	public void setRessourcesBase(ArrayList<Ressources> listRessources) {
+		this.listRessources = listRessources;
 	}
 
 	// ajoute l'objet dans la liste
 	public void ajouterRessources(Ressources r) {
-		this.ressourcesBase.put(r, this.ressourcesBase.get(r)+1);
+		this.listRessources.add(r);
 	}
 	
 	
 	// supprime l'objet de la list
 	public void supprimmeRessources(Ressources r) {
-		this.ressourcesBase.put(r, this.ressourcesBase.get(r)-1);
+		this.listRessources.remove(r);
 	}
 	
 	// dit si l'objet est dans la list
 	public boolean contientRessources(Ressources r) {
-		return this.ressourcesBase.get(r)>0;
+		return this.listRessources.contains(r);
 	}
 
 }
