46201f2f76ac0d907f620bff977460ac9f7f2841
SoleneLct <101125740+SoleneLct@users.noreply.github.com>
2022-05-25 11:11:54 +0200

    Update Ressources.java

--- TerrainJeu/src/applicationV1/modele/Ressources.java
+++ TerrainJeu/src/applicationV1/modele/Ressources.java
@@ -1,11 +1,46 @@
 package applicationV1.modele;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public class Ressources {
-	
+
 	private int pv;
-	private Personnage p;
+	private int donnerPV;
+	private IntegerProperty quantiteProperty;
 	
-	public Ressources(int pv) {
-		this.pv=pv;
+	public Ressources(int pv,int donnerPV) {
+		this.pv = pv;
+		this.donnerPV = donnerPV;
+		this.quantiteProperty =  new SimpleIntegerProperty(0);
 	}
+
+	public int getPv() {
+		return pv;
+	}
+
+	public void setPv(int pv) {
+		this.pv = pv;
+	}
+
+	public int getDonnerPV() {
+		return donnerPV;
+	}
+
+	public void setDonnerPV(int donnerPV) {
+		this.donnerPV = donnerPV;
+	}
+	
+	public final IntegerProperty quantiteProperty() {
+		return quantiteProperty;
+	}
+	
+	public IntegerProperty getQuantiteProperty() {
+		return quantiteProperty;
+	}
+
+	public void setQuantiteProperty(IntegerProperty quantiteProperty) {
+		this.quantiteProperty = quantiteProperty;
+	}
+
 }
