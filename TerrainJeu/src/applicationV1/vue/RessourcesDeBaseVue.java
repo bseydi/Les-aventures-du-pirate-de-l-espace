4453fb0ba9581b160d7d483e8af40d5cf168cf07
Aldriculteur <105204520+Aldriculteur@users.noreply.github.com>
2022-05-31 11:09:58 +0200

    Update RessourcesDeBaseVue.java

--- TerrainJeu/src/applicationV1/vue/RessourcesDeBaseVue.java
+++ TerrainJeu/src/applicationV1/vue/RessourcesDeBaseVue.java
@@ -1,39 +1,28 @@
 package applicationV1.vue;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Map;
 
 import applicationV1.modele.Ressources;
-import javafx.beans.property.IntegerProperty;
-import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.TilePane;
 
 public class RessourcesDeBaseVue {
 	
-	private TilePane placeRessources;
 	private Label labelBois;
 	private Label labelFer;
 	private Label labelPierre;	
-	//private Map<String,Integer> ressourcesBase;
 	private ArrayList<Ressources> listRessources;
 	private Ressources bois;
 	private Ressources fer;
 	private Ressources pierre;
 	
 	public RessourcesDeBaseVue(TilePane placeRessource,Label labelBois,Label labelFer,Label labelPierre,ArrayList<Ressources> listRessources) {
-		this.placeRessources=placeRessource;
 		this.labelBois=labelBois;
 		this.labelFer=labelFer;
 		this.labelPierre=labelPierre;
-		//this.labelRessources=labelRessources;
 		this.listRessources=listRessources;
-		//this.ressourcesBase=ressourcesBase;
 	}
 	
 	public void afficheRessources() throws FileNotFoundException {
@@ -45,16 +34,16 @@ public class RessourcesDeBaseVue {
 	
 	public void bois() throws FileNotFoundException{
 		this.bois=listRessources.get(0);
-		labelBois.textProperty().bind(this.bois.getQuantiteProperty().asString());           
+		labelBois.textProperty().bind(this.bois.getNbBoisProperty().asString());           
 	}
 	
 	public void fer() throws FileNotFoundException{
 		this.fer=listRessources.get(1);
-		labelFer.textProperty().bind(this.fer.getQuantiteProperty().asString());
+		labelFer.textProperty().bind(this.fer.getNbFerProperty().asString());
 	}
 	
 	public void pierre() throws FileNotFoundException{
 		this.pierre=listRessources.get(3);
-		labelPierre.textProperty().bind(this.pierre.getQuantiteProperty().asString());
+		labelPierre.textProperty().bind(this.pierre.getNbPierreProperty().asString());
 	}
 }
