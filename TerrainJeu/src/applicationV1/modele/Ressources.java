989565a996bf8e8f10889fd8f2385ce4c0367448
Aldriculteur <105204520+Aldriculteur@users.noreply.github.com>
2022-05-31 11:47:08 +0200

    Update Ressources.java

--- TerrainJeu/src/applicationV1/modele/Ressources.java
+++ TerrainJeu/src/applicationV1/modele/Ressources.java
@@ -5,34 +5,14 @@ import javafx.beans.property.SimpleIntegerProperty;
 
 public class Ressources {
 
-	private int pv;
-	private int donnerPV;
 	private IntegerProperty nbBoisProperty;
 	private IntegerProperty nbPierreProperty;
 	private IntegerProperty nbFerProperty;
 	
-	public Ressources(int pv,int donnerPV) {
-		this.pv = pv;
-		this.donnerPV = donnerPV;
+	public Ressources() {
 		this.nbBoisProperty =  new SimpleIntegerProperty(1);
 		this.nbPierreProperty =  new SimpleIntegerProperty(2);
 		this.nbFerProperty =  new SimpleIntegerProperty(3);
-	}
-
-	public int getPv() {
-		return pv;
-	}
-
-	public void setPv(int pv) {
-		this.pv = pv;
-	}
-
-	public int getDonnerPV() {
-		return donnerPV;
-	}
-
-	public void setDonnerPV(int donnerPV) {
-		this.donnerPV = donnerPV;
 	}
 
 	public IntegerProperty getNbBoisProperty() {
