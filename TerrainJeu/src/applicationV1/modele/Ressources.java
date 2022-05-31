4b56cc0c1109e3ebb279e84df1bdd74197648697
Aldriculteur <105204520+Aldriculteur@users.noreply.github.com>
2022-05-31 14:57:38 +0200

    Update Ressources.java

--- TerrainJeu/src/applicationV1/modele/Ressources.java
+++ TerrainJeu/src/applicationV1/modele/Ressources.java
@@ -6,32 +6,61 @@ import javafx.beans.property.SimpleIntegerProperty;
 public class Ressources {
 
 	private IntegerProperty nbBoisProperty;
-	private IntegerProperty nbPierreProperty;
+	private IntegerProperty nbPierreProperty ;
 	private IntegerProperty nbFerProperty;
 	
 	public Ressources() {
-		this.nbBoisProperty =  new SimpleIntegerProperty(1);
-		this.nbPierreProperty =  new SimpleIntegerProperty(2);
-		this.nbFerProperty =  new SimpleIntegerProperty(3);
+		this.nbBoisProperty =  new SimpleIntegerProperty(0);
+		this.nbPierreProperty =  new SimpleIntegerProperty(0);
+		this.nbFerProperty =  new SimpleIntegerProperty(0);
 	}
 
-	public IntegerProperty getNbBoisProperty() {
-		return this.nbBoisProperty;
+	public final int getNbPierre() {
+		return this.nbPierreProperty.getValue();
 	}
-	public IntegerProperty getNbPierreProperty() {
-		return this.nbPierreProperty;
-	}
-	public IntegerProperty getNbFerProperty() {
-		return this.nbFerProperty;
-	}
-	public void setBoisProperty(int quantite) {
-		this.nbBoisProperty.setValue(quantite);
-	}
-	public void setPierreProperty(int quantite) {
+	public final void setNbPierre(int quantite) {
 		this.nbPierreProperty.setValue(quantite);
 	}
-	public void setFerProperty(int quantite) {
+	public final IntegerProperty nombrePierreProperty() {
+		return this.nbPierreProperty;
+	}
+	public final void ajoutPierre(int quantite) {
+		setNbPierre(getNbPierre() + quantite);
+	}
+	public final void retirerPierre(int quantite) {
+		setNbPierre(getNbPierre() - quantite);
+	}
+	
+	public final int getNbBois() {
+		return this.nbBoisProperty.getValue();
+	}
+	public final void setNbBois (int quantite) {
+		this.nbBoisProperty.setValue(quantite);
+	}
+	public final IntegerProperty nombreBoisProperty() {
+		return this.nbBoisProperty;
+	}
+	public final void ajoutBois(int quantite) {
+		setNbBois(getNbBois() + quantite);
+	}
+	public final void retirerBois(int quantite) {
+		setNbBois(getNbBois() - quantite);
+	}
+	
+	public final int getNbFer () {
+		return this.nbFerProperty.getValue();
+	}
+	public final void setNbFer (int quantite) {
 		this.nbFerProperty.setValue(quantite);
+	}
+	public final IntegerProperty nombreFerProperty() {
+		return this.nbFerProperty;
+	}
+	public final void ajoutFer (int quantite ) {
+		setNbFer(getNbFer() + quantite);
+	}
+	public final void retirerFer (int quantite) {
+		setNbFer(getNbFer() - quantite);
 	}
 	
 }
