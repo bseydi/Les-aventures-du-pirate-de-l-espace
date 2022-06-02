6ec48089db6753ce6baf8f176c63f1c05e9836d3
WoodnycaV <101125564+WoodnycaV@users.noreply.github.com>
2022-06-02 12:16:17 +0200

    Update VieVue.java
    update de la class VieVue

--- TerrainJeu/src/applicationV1/vue/VieVue.java
+++ TerrainJeu/src/applicationV1/vue/VieVue.java
@@ -1,8 +1,7 @@
 package applicationV1.vue;
 
 
-import java.io.FileNotFoundException;
-
+import javafx.beans.property.IntegerProperty;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
@@ -11,48 +10,57 @@ import javafx.scene.layout.Pane;
 public class VieVue {
 	
 	private Pane placeCoeur;
-	private int vie;
+	private IntegerProperty vie;
+	private Image[] tabImg;
 	
-	public VieVue(Pane placeCoeur,int vie ) {
+	public VieVue(Pane placeCoeur,IntegerProperty vie ) {
 		this.placeCoeur = placeCoeur;
-		this.vie = vie;
-
+		this.vie =  vie;
+		tabImg = new Image[8];
+		
+		
+		
+		tabImg[0] = new Image("./image/imageCoeurs/coeur100.png");
+		tabImg[1] = new Image("./image/imageCoeurs/coeur90.png");
+		tabImg[2] = new Image("./image/imageCoeurs/coeur75.png");
+		tabImg[3] = new Image("./image/imageCoeurs/coeur60.png");
+		tabImg[4] = new Image("./image/imageCoeurs/coeur45.png");
+		tabImg[5] = new Image("./image/imageCoeurs/coeur30.png");
+		tabImg[6] = new Image("./image/imageCoeurs/coeur15.png");
+		tabImg[7] = new Image("./image/imageCoeurs/coeur0.png");
+		
 	} 
 	
-	public void afficheCoeur() throws FileNotFoundException {
+	public void afficheCoeur()  {
+		ImageView img = new ImageView(tabImg[0]);
 		
-		ImageView img = new ImageView(new Image("./image/imageCoeurs/coeur100.png"));
-		
-		switch(vie) {
-		case 100:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur100.png"));
-			break;
-		case 90:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur90.png"));
-			break;
-		case 75:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur75.png"));
-			break;
-		case 60:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur60.png"));
-			break;
-		case 45:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur45.png"));
-			break;
-		case 30:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur30.png"));
-			break;
-		case 15:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur15.png"));
-			break;
-		case 0:
-			img = new ImageView(new Image("./image/imageCoeurs/coeur0.png"));
-			break;
-		default:
-			break;
+		if(vie.get() == 0) {
+			img = new ImageView(tabImg[7]);
+		}
+		else if(vie.get() > 0 && vie.get() <= 15) {
+			img = new ImageView(tabImg[6]);
+		}
+		else if (vie.get() <= 30) {
+			img = new ImageView(tabImg[5]);
+		}
+		else if( vie.get() <= 45) {
+			img = new ImageView(tabImg[4]);
+		}
+		else if(vie.get() <= 60) {
+			img = new ImageView(tabImg[3]);
+		}
+		else if (vie.get() <= 75) {
+			img = new ImageView(tabImg[2]);
+		}
+		else if( vie.get() <= 90) {
+			img = new ImageView(tabImg[1]);
+
+		}
+		else if( vie.get() <=100) {
+			img = new ImageView(tabImg[0]);
 		}
 		placeCoeur.getChildren().add(img);
-	
+		
 	}
 	
 	
